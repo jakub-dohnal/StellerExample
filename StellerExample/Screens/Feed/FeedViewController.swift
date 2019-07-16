d7e9e343c25ea50caaedcaa4d32575696b84493a
Jakub Dohnal <dohnal.jakub@me.com>
2019-07-16 22:50:36 +0200

    FeedViewController

---
+++ StellerExample/Screens/Feed/FeedViewController.swift
@@ -0,0 +1,60 @@
+//
+//  FeedViewController.swift
+//  StellerExample
+//
+//  Created by Jakub Dohnal on 16/07/2019.
+//  Copyright © 2019 Jakub Dohnal. All rights reserved.
+//
+
+import UIKit
+
+private let reuseIdentifier = "Cell"
+
+class FeedViewController: UICollectionViewController {
+
+    let storiesViewModels: [String]
+
+    init(stories: [String]) {
+        storiesViewModels = stories
+
+        super.init(collectionViewLayout: UICollectionViewFlowLayout())
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.collectionView!.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
+    }
+
+    // MARK: UICollectionViewDataSource
+
+    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of items
+        return storiesViewModels.count
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
+    
+        // Configure the cell
+    
+        return cell
+    }
+
+    // MARK: UICollectionViewDelegate
+
+    // Uncomment this method to specify if the specified item should be selected
+    override func collectionView(_ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+}
