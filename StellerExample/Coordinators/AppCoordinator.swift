915bb5b776d577137270545b6ca2edbb1e3cdb60
Jakub Dohnal <dohnal.jakub@me.com>
2019-07-16 22:10:14 +0200

    coordinator
    	- AppCoordinator

---
+++ StellerExample/Coordinators/AppCoordinator.swift
@@ -0,0 +1,27 @@
+//
+//  AppCoordinator.swift
+//  StellerExample
+//
+//  Created by Jakub Dohnal on 16/07/2019.
+//  Copyright © 2019 Jakub Dohnal. All rights reserved.
+//
+
+import Foundation
+
+final class AppCoordinator: Coordinator {
+    let router: Router
+
+    init(router: Router) {
+        self.router = router
+    }
+
+    func start() {
+        showFeed()
+    }
+}
+
+private extension AppCoordinator {
+    func showFeed() {
+        //TODO: present feed screen
+    }
+}
---
+++ StellerExample/Coordinators/Coordinator.swift
@@ -0,0 +1,13 @@
+//
+//  Coordinator.swift
+//  StellerExample
+//
+//  Created by Jakub Dohnal on 16/07/2019.
+//  Copyright © 2019 Jakub Dohnal. All rights reserved.
+//
+
+import Foundation
+
+protocol Coordinator {
+    func start()
+}
