1c3baa57b0ec67de47ed4c72c8b1be9378518bf5
Jakub Dohnal <dohnal.jakub@me.com>
2019-07-16 22:51:14 +0200

    FeedCell

---
+++ StellerExample/Screens/Feed/FeedCell.swift
@@ -0,0 +1,20 @@
+//
+//  FeedCell.swift
+//  StellerExample
+//
+//  Created by Jakub Dohnal on 16/07/2019.
+//  Copyright © 2019 Jakub Dohnal. All rights reserved.
+//
+
+import UIKit
+
+class FeedCell: UICollectionViewCell {
+
+    @IBOutlet private weak var storyImageView: UIImageView!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
