fe03ca539c8a51c3f96e4839c0e64a1da0b3751b
Jakub Dohnal <dohnal.jakub@me.com>
2019-07-18 20:22:21 +0200

    Feed cell image placeholder

--- StellerExample/Screens/Feed/FeedCell.swift
+++ StellerExample/Screens/Feed/FeedCell.swift
@@ -13,15 +13,26 @@ class FeedCell: RegistrableCell {
     @IBOutlet private weak var storyImageView: UIImageView!
     private var imageTask: URLSessionDataTask?
 
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        setupUI()
+    }
+
     func config(viewModel: StoryViewModel) {
         imageTask = storyImageView.image(remoteURL: viewModel.coverURL)
-
-        backgroundColor = UIColor(white: 0.5, alpha: 0.5)
-        layer.cornerRadius = 5
+        storyImageView.image = UIImage(named: "placeholder")
     }
 
     override func prepareForReuse() {
         imageTask?.cancel()
         storyImageView.image = nil
+    }
+}
+
+private extension FeedCell {
+    func setupUI() {
+        layer.cornerRadius = 5
+        layer.borderWidth = 3
+        layer.borderColor = UIColor(white: 0.5, alpha: 0.5).cgColor
     }
 }
